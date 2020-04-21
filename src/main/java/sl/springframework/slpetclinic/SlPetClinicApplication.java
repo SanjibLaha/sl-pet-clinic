477833d4f2e0c03c1c5e2e6c17da1f5e9c6d4724
Sanjib Laha <>
2020-04-21 20:25:16 +0530

    First Check in for pet application

---
+++ src/main/java/sl/springframework/slpetclinic/SlPetClinicApplication.java
@@ -0,0 +1,13 @@
+package sl.springframework.slpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sl/springframework/slpetclinic/SlPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sl.springframework.slpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
